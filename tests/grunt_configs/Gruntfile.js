f87bd80ee57481dfbbfbcdcff5a0225e371464ca
Jim Doyle <jimdoyle82@gmail.com>
2015-01-22 22:19:41 +1100

    added some unit tests (test1)

--- tests/grunt_configs/Gruntfile.js
+++ tests/grunt_configs/Gruntfile.js
@@ -1,15 +1,20 @@
+/**
+ * This Gruntfile is meant for unit testing and should be executed within a
+ * Jasmine `*_spec.js` file, by using the `shell.exec` library to initialize Grunt.
+ * There is no default task because each target gets specified in the shell command.
+ */
+
 'use strict';
 module.exports = function(grunt) {
 
 	grunt.initConfig({
 		rootDir: process.cwd() + "/../../"
 		,badass: {
+			// Load in different targets
 			test1: require("./test1.js").test
 		}
 	});
 
-	// Actually load this plugin's task(s).
+	// Loads in grunt-badass
 	grunt.loadTasks( grunt.config("rootDir")+'tasks');
-
-	grunt.registerTask('default', ['badass']);
 };
--- tests/main_spec.js
+++ tests/main_spec.js
@@ -2,24 +2,60 @@
 
 var badass = require("../tasks/badass.js");
 var shell = require("shelljs");
+var fse = require("fs-extra");
+var cwd = process.cwd();
 
-describe("badass", function() {
+describe("test 1 - check generated files and folders", function() {
+	
+	var config = require("./grunt_configs/test1.js").test
+		,TEST_DIR = "./dist/test1/"
+		,pngDir = TEST_DIR+config.options.pngDir+"/"
+		,singlesDir = TEST_DIR+"singles/";
+	
+	gruntTest(1);
 
-	beforeEach(function() {
+	it("should have created a scss file for icons that no longer contains any template syntax.", function() {		
+		expect( fse.existsSync("./dist/test1/icons.scss") ).toBe( true );
 
-		var cwd = process.cwd();
-		process.chdir("tests/grunt_configs/");
-		var result = shell.exec("grunt badass", {silent:true});
-		process.chdir(cwd);
-
-		console.log( "before each", result );
+		var scss = fse.readFileSync(TEST_DIR+"icons.scss").toString();
+		expect( scss.indexOf("<%=") ).toEqual(-1);
 	});
 
-	afterEach(function() {
-		console.log( "after each" );
+	it( "should check that all SVG icons have had corresponding PNGs generated", function() {
+
+		expect( fse.existsSync(pngDir) ).toBe( true );
+		
+		config.options.items.forEach( function(item, i) {
+			var pngIcon = pngDir+item.class+".png";
+			expect( fse.existsSync(pngIcon) ).toBe( true );
+		});
 	});
 
-	it("should be true", function() {
-		expect(true).toBe(true);
+	it("should check that specified stand alone pngs have been generate", function() {
+		
+		expect( fse.existsSync(TEST_DIR+"singles/") ).toBe( true );
+
+		config.options.items.forEach( function(item, i) {
+			if( item.standAlone ) 
+				expect( fse.existsSync( singlesDir+item.class+".png" ) ).toBe( true );
+		});
+	});
+
+	/*it("should have copied the `svgloader.js` file into dist.", function() {		
+		expect( fse.existsSync("./dist/test1/svgloader.js") ).toBe( true );
+	});*/
+
+});
+
+describe("cleanup", function() {
+	it("should clean up the dist folder", function() {
+		fse.removeSync( "./dist" );
+		expect( fse.existsSync("./dist") ).toBe( false );
 	});
 });
+
+function gruntTest( number ) {
+	process.chdir("tests/grunt_configs/");
+	var result = shell.exec("grunt badass:test"+number, {silent:true});
+	process.chdir(cwd);
+}
